330695f28e0dbdf95c72acbdaad4d8a7ce813cc1
MedadRufus <medadrufus@gmail.com>
2020-03-06 19:27:24 +0000

    submitted

--- Circuits/Button_blink/Button_blink.ino
+++ Circuits/Button_blink/Button_blink.ino
@@ -23,29 +23,50 @@
 */
 
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 11;     // the number of the pushbutton pin
+const int buttonPin = 9;// the number of the pushbutton pin
+const int buttonPin1 = 11;
 const int ledPin =  13;      // the number of the LED pin
+int left_wire = 5;
+int right_wire = 3;
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
+int buttonState1 = 0; 
 
 void setup() {
+  Serial.begin(9600); // open the serial port at 9600 bps
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+  pinMode(left_wire, OUTPUT);
+  pinMode(right_wire, OUTPUT);
 }
 
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
+  buttonState1 = digitalRead(buttonPin1);
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
+  if (buttonState == LOW) {
     // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } else {
+    digitalWrite(left_wire, HIGH);
+    //digitalWrite(ledPin, HIGH);}
+    Serial.print("pressed");
+  }
+  else if (buttonState1 == LOW) {
+    // turn LED on:
+   // digitalWrite(ledPin, HIGH);
+   digitalWrite(right_wire, HIGH);
+   Serial.print("pressed 1");}
+
+    
+   else {
     // turn LED off:
     digitalWrite(ledPin, LOW);
+    digitalWrite(right_wire, LOW);
+    digitalWrite(left_wire, LOW);
+    Serial.print("all low");
   }
 }
